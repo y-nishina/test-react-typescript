98bf71f3797462b818c392095c8fc9a6f70d14ea
yuta nishina <y.nishina0509@gmail.com>
2020-11-17 00:33:46 +0900

    ESLintの適用ルールをカスタマイズ

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,13 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -13,12 +20,56 @@ module.exports = {
       jsx: true,
     },
     ecmaVersion: 12,
+    project: './tsconfig.eslint.json',
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   plugins: [
-    'react',
     '@typescript-eslint',
+    'import', 
+    'jsx-a11y',
+    'react',
+    'react-hooks',
   ],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages', {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      }
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx']
+      }
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
   },
+  overrides: [
+    {
+      'files': ['*.tsx'],
+      'rules': {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+  root:true,
 };
